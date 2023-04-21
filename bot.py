5a04c77f6afe803c692010e99699cdd08b65f5fa
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-04-21 01:47:17 -0500

    MUCH more efficient history management - dual API call to summarize with GPT-3 after main GPT-4 message

--- bot.py
+++ bot.py
@@ -2,18 +2,28 @@ import openai
 import os
 from dotenv import load_dotenv
 
-class Troubleshooter:
+class Bot:
     def __init__(self):
         load_dotenv()
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    def get_troubleshooting_suggestion(self, conversation):
+    def get_suggestion(self, conversation):
         response = openai.ChatCompletion.create(
             model="gpt-4",
             max_tokens = 1024,
             messages=[
                 {"role": "system", "content": "You are a Ruby, a witty, charming conversationalist. Answer concisely. Answer in sentences. Break long responses into smaller parts and confirm the user would like to continue."}
             ] + conversation
+        )
+        return response['choices'][0]['message']['content']
+    
+    def summarize(self, text):
+        response = openai.Completion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": "Summarize the following text."},
+                {"role": "user", "content": text},
+            ]
         )
         return response['choices'][0]['message']['content']
     
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, send_from_directory, session, send_file
 from flask_session import Session
 from twilio.twiml.voice_response import Gather, VoiceResponse
 from transcription import Transcriber
-from troubleshooting import Troubleshooter
+from bot import Bot
 import os
 import json
 
@@ -18,12 +18,13 @@ class Assistant:
     def __init__(self, call_sid):
         self.call_sid = call_sid
         self.transcriber = Transcriber()
-        self.troubleshooter = Troubleshooter()
+        self.bot = Bot()
 
     def process_audio(self, conversation):
         if conversation:
-            suggestion = self.troubleshooter.get_troubleshooting_suggestion(conversation)
-            print("Troubleshooting Suggestion:", suggestion)
+            print("User:", conversation[-1]['content'])
+            suggestion = self.bot.get_suggestion(conversation)
+            print("Response:", suggestion)
             return suggestion
 
 assistants = {}
@@ -34,7 +35,7 @@ def start_call():
     session['call_sid'] = call_sid
 
     response = VoiceResponse()
-    response.say("Hi, I'm Ruby the AI! If I get stuck, just say the continue. How can I help you today?")
+    response.say("Hi, I'm Ruby the AI! If I get stuck, just say continue. How can I help you today?")
     gather = Gather(input='speech', action='/process_speech', speechTimeout="auto", speechModel="phone_call")
     response.append(gather)
     response.redirect('/process_speech')
@@ -56,10 +57,12 @@ def process_speech():
             os.makedirs(f"conversations/{call_sid}")
       
         #if transcript file exists, read it
+        limit = 8
+
         if os.path.exists(os.path.join(f"conversations/{call_sid}", "transcript.txt")):
             with open(os.path.join(f"conversations/{call_sid}", "transcript.txt"), "r") as f:
                 lines = f.readlines()
-                conversation = [json.loads(line) for line in lines]
+                conversation = [json.loads(line) for line in lines[-limit:]]
         else:
             conversation = []
 
@@ -68,8 +71,11 @@ def process_speech():
         if not user_text:
 
             # If the user didn't say anything, log it in the transcript.txt file
+            user_message = {"role": "user", "content": "No speech detected."}
+            ai_message = {"role": "system", "content": "I'm sorry, I didn't quite catch that."}
             with open(os.path.join(f"conversations/{call_sid}", "transcript.txt"), "a") as f:
-                f.write(f"\nUser: *pauses* \nAI: I'm sorry, I didn't quite catch that.")
+                f.write(json.dumps(user_message) + "\n")
+                f.write(json.dumps(ai_message) + "\n")
 
             response = VoiceResponse()
             response.say("I'm sorry, I didn't quite catch that.")
@@ -84,9 +90,12 @@ def process_speech():
         # Process the audio and get the suggestion
         suggestion = assistants[call_sid].process_audio(conversation)
 
+        # Summarize the AI response
+        summary = assistants[call_sid].bot.summarize(suggestion)
+
         # Save the user and AI messages in the transcript.txt file
         user_message = {"role": "user", "content": user_text}
-        ai_message = {"role": "system", "content": suggestion}
+        ai_message = {"role": "system", "content": summary}
         with open(os.path.join(f"conversations/{call_sid}", "transcript.txt"), "a") as f:
             f.write(json.dumps(user_message) + "\n")
             f.write(json.dumps(ai_message) + "\n")
